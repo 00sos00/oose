76576575fe67b70f7af7bd64e606e3eb2492b7d2
sxsb <42996937+00sos00@users.noreply.github.com>
2025-04-19 00:29:48 +0200

    Sign in page

---
+++ front/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sign In</title>
+	<link rel="stylesheet" href="index.css">
+	<link rel="stylesheet" href="f.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Arabic:wdth,wght@62.5..100,100..900&family=Roboto:ital,wdth,wght@0,75..100,100..900;1,75..100,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+	<form action="/" id="sign-holder">
+		<h1>Sign In</h1>
+		<label for="username" class="sign-label">Username</label>
+		<input name="username" type="text" class="sign-input">
+		<label for="password" class="sign-label">Password</label>
+		<input name="password" type="password" class="sign-input">
+		<a href="/">Forgot your password?</a>
+		<button type="submit">Sign In</button>
+	</form>
+</body>
+
+</html>
