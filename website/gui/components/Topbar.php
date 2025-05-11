7033098316b41a09149b513db971c84402bc5463
Safa115 <safa.abdelkarim@msa.edu.eg>
2025-05-11 22:37:09 +0300

    add Topbar component

--- website/gui/components/Topbar.php
+++ website/gui/components/Topbar.php
@@ -4,9 +4,9 @@ $strippedFileName = basename(__FILE__, ".php");
 $gui->addComponentRenderFunction($strippedFileName, function ($props) {
     ob_start();
 ?>
-    <div class="topbar">
-        <div class="topbar-right">
-            <img src="assets/notification-icon.svg" alt="Notifications" class="topbar-icon" />
+    <div class="Topbar">
+        <div class="Topbar-right">
+            <img src="assets/notification-icon.svg" alt="Notifications" class="Topbar-icon" />
 
             <div class="user-info">
                 <p class="user-name"><?= $props["user-name"] ?? "User Name" ?></p>
