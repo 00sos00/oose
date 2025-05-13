9e1e0800f8528e5a97bb90151e0d4e47d8a1d437
sxsb <42996937+00sos00@users.noreply.github.com>
2025-05-13 11:43:00 +0300

    Check signed-in logic

--- website/accounts/index.php
+++ website/accounts/index.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . "/../controllers/check-signed.php";
 require_once __DIR__ . "/../Model/Database.php";
 require_once __DIR__ . "/../gui/GUI.php";
 $db = DataBase::getInstance();
